e103596614525dd772415c4cc915e2847dc6f99c
Daria <djj2115cu@gmail.com>
2015-06-26 14:48:30 -0700

    update gulpfile so unit/e2e tests can be written in TS

---
+++ test/e2e/test.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/mocha/mocha.d.ts"/>
+
+var assert = require('assert');
+
+
+describe('Hello World', function(){
+  it('The first char of "Hello World" should be "H"', function(){
+    assert.equal('H', "Hello World"[0]);
+  })
+})
